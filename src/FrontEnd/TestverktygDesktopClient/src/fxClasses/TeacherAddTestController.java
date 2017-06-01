a48b5fd8288a5e4e72482608b70e27011ccc2154
magnus <magnusnorlin@gmail.com>
2017-06-01 15:53:30 +0200

    TeacherAddTest, skriver inte till DB, fel på skapat TEST kanske.
    Louise och Magnus

--- src/FrontEnd/TestverktygDesktopClient/src/fxClasses/TeacherAddTestController.java
+++ src/FrontEnd/TestverktygDesktopClient/src/fxClasses/TeacherAddTestController.java
@@ -18,7 +18,12 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import models.Question;
+import models.Test;
 
 /**
  * FXML Controller class
@@ -51,6 +56,8 @@ public class TeacherAddTestController implements Initializable
     @FXML
     private TextField textFieldTimeLeft;
     @FXML
+    private TextField textFieldTestName;
+    @FXML
     private TextField textFieldQuestionText;
     @FXML
     private TextField textFieldQuestionAlt1;
@@ -77,14 +84,20 @@ public class TeacherAddTestController implements Initializable
     @FXML
     private Button btnDone;
 
-    List<Question> newTest = new ArrayList();
+    List<models.Question> newTest = new ArrayList();
 
     int chosenCheckBoxId = 0;
+    int questionNumCount = 1;
+
+    int isAutoCorrected;
+
+    Client client;
 
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        //setLabelsOfTeacher();
+        client = ClientBuilder.newClient();
+        labelQuestionNr.setText("Fråga nummer " + questionNumCount);
 
     }
 
@@ -166,7 +179,7 @@ public class TeacherAddTestController implements Initializable
         if (chosenCheckBoxId > 0)
         {
             q.setCorrectAnswer(chosenCheckBoxId);
-            
+
             newTest.add(q);
 
             textFieldQuestionText.clear();
@@ -184,12 +197,51 @@ public class TeacherAddTestController implements Initializable
             System.out.println("Felmeddelande i GUI");
         }
 
+        questionNumCount++;
+
+    }
+
+    @FXML
+    private void checkBoxHandlerAutoCorrectedTest(MouseEvent event)
+    {
+        String source1 = event.getSource().toString();
+        System.out.println("Event source---------->>>>>><<<<<<<----------" + source1);
+        switch (source1)
+        {
+            case "CheckBox[id=chckBxYes, styleClass=check-box]'Ja'":
+            {
+                chckBxYes.selectedProperty().set(true);
+                chckBxNo.selectedProperty().set(false);
+                isAutoCorrected = 1;
+                break;
+            }
+
+            case "CheckBox[id=chckBxNo, styleClass=check-box]'Nej'":
+            {
+                chckBxNo.selectedProperty().set(true);
+                chckBxYes.selectedProperty().set(false);
+                isAutoCorrected = 0;
+                break;
+            }
+        }
+
     }
 
     @FXML
     private void btnDoneAction(ActionEvent event)
     {
-        
+        models.Test t = new models.Test();
+        t.setName(textFieldTestName.getText());
+        t.setAutoCorrectedTest(isAutoCorrected);
+        t.setSubject("Magnus testSubject");
+        //t.setSubject(LoginController.currentTeacher.getSubject());
+        t.setTotalTime(Integer.parseInt(textFieldTimeLeft.getText()));
+        t.setQuestions(newTest);
+
+        Test taSK = client.target("http://localhost:8080/TestverktygDesktop/webapi/tests/")                
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(t), Test.class);
+
     }
 
 }
