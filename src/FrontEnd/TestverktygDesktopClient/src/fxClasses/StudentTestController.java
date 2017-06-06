d6bf04aae3dc522b719972629c48073568a08707
Henrik <henrik.rosqvist95@gmail.com>
2017-06-06 21:21:18 +0200

    StudentTestController
    Lagt till metoder och städat lite i klassen, databas-delen är inte gjord än

--- src/FrontEnd/TestverktygDesktopClient/src/fxClasses/StudentTestController.java
+++ src/FrontEnd/TestverktygDesktopClient/src/fxClasses/StudentTestController.java
@@ -16,6 +16,7 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -40,52 +41,45 @@ import repositories.StudentAnswerRepository;
 public class StudentTestController implements Initializable {
 
     QuestionRepository qr = new QuestionRepository();
-    List<StudentAnswer> answers = new ArrayList();
-    List<models.Question> questions = qr.getQuestions();
     ObservableList<Question> observableQuestions = FXCollections.observableArrayList();
+    List<StudentAnswer> answers = new ArrayList();
+    List<models.Question> questions;
     Question currentQ;
-    final Integer startTime = 10;
     Timeline timeline = null;
-    IntegerProperty timeSeconds = new SimpleIntegerProperty(startTime);
+    Test currentTest;
 
-    Test currentTest = StudentStartPageController.currentTest;
-
-    int chosenCheckBoxId = 0;
+    Integer startTime;
+    IntegerProperty timeSeconds;
+    int chosenCheckBoxId;
     String emptyUrl = "http://www.jennybeaumont.com/wp-content/uploads/2015/03/placeholder-800x423.gif";
 
-    @FXML
-    private Label lblTestSubject, lblTimeLeft, lblTeacherSubject, lblcurrentQuestionNR, lblQuestionText;
-    @FXML
-    private Label quealternativ1, quealternativ2, quealternativ3, quealternativ4;
+    @FXML private Label lblTestSubject, lblTimeLeft, lblTeacherSubject, lblcurrentQuestionNR, lblQuestionText;
+    @FXML private Label quealternativ1, quealternativ2, quealternativ3, quealternativ4;
     @FXML private Label lblCorrectAnswers;
-    @FXML
-    private ListView lvQuestionsNR;
-    @FXML
-    private ListView lvCorrectedQuestion;
-    @FXML
-    private CheckBox chbxalternativ1;
-    @FXML
-    private CheckBox chbxalternativ3;
-    @FXML
-    private CheckBox chbxalternativ4;
-    @FXML
-    private CheckBox chbxalternativ2;
-    @FXML
-    private ImageView ivImage;
-    @FXML
-    private Button btnSaveTest, btnDone;
+    
+    @FXML private ListView lvQuestionsNR;
+    @FXML private ListView lvCorrectedQuestion;
+    
+    @FXML private CheckBox chbxalternativ1;
+    @FXML private CheckBox chbxalternativ3;
+    @FXML private CheckBox chbxalternativ4;
+    @FXML private CheckBox chbxalternativ2;
+    
+    @FXML private ImageView ivImage;
+    @FXML private Button btnSaveTest, btnDone;
 
-    public void populateListView()
+    private void populateListView()
     {
         //populate listview
-        System.out.println(questions.size());
+        System.out.println("Antalet frågor i provet: " + questions.size());
         for (int i = 0; i < questions.size(); i++)
         {
-            //      if (currentTest.getId()== questions.get(i).getTest_Id()) {
-            observableQuestions.add(new Question(questions.get(i).getId(), questions.get(i).getqText(),
-                    questions.get(i).getCorrectAnswer(), questions.get(i).getImageURL(),
-                    questions.get(i).getAnswers(), questions.get(i).getTest_Id()));
-            //    }
+                if (currentTest.getId()== questions.get(i).getTest_Id()) 
+                {
+                    models.Question q = questions.get(i);
+                    observableQuestions.add(new Question(q.getId(), q.getqText(), q.getCorrectAnswer(),
+                        q.getImageURL(), q.getAnswers(), q.getTest_Id()));
+                }
         }
 
         lvQuestionsNR.setItems(observableQuestions);
@@ -125,15 +119,23 @@ public class StudentTestController implements Initializable {
     }
     
     @FXML
-    private void btnDoneClicked()
+    private void btnDoneClicked(ActionEvent event) throws IOException
     {
         StudentAnswerRepository saRepo = new StudentAnswerRepository();
         
         
+        //Stänger scenen och går tillbaka
+        Parent studentTestParent = FXMLLoader.load(getClass().getResource("StudentStartPage.fxml"));
+        Scene studentTestScene = new Scene(studentTestParent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(studentTestScene);
+        window.show();
+    
     }
     
     @FXML
-    public void checkBoxHandler(MouseEvent event)
+    private void checkBoxHandler(MouseEvent event)
     {
         String source1 = event.getSource().toString().substring(12, 27);
         System.out.println(source1);
@@ -142,63 +144,47 @@ public class StudentTestController implements Initializable {
             case "chbxalternativ1":
             {
                 chbxalternativ1.selectedProperty().set(true);
-
-                chbxalternativ2.selectedProperty().set(false);
-                chbxalternativ3.selectedProperty().set(false);
-                chbxalternativ4.selectedProperty().set(false);
                 chosenCheckBoxId = 1;
 
                 saveDoneQuestion();
-                emptyCheckboxes();
+                emptyCheckBoxes(true);
                 break;
             }
 
             case "chbxalternativ2":
             {
                 chbxalternativ2.selectedProperty().set(true);
-
-                chbxalternativ1.selectedProperty().set(false);
-                chbxalternativ3.selectedProperty().set(false);
-                chbxalternativ4.selectedProperty().set(false);
                 chosenCheckBoxId = 2;
 
                 saveDoneQuestion();
-                emptyCheckboxes();
+                emptyCheckBoxes(true);
                 break;
             }
 
             case "chbxalternativ3":
             {
                 chbxalternativ3.selectedProperty().set(true);
-
-                chbxalternativ1.selectedProperty().set(false);
-                chbxalternativ2.selectedProperty().set(false);
-                chbxalternativ4.selectedProperty().set(false);
                 chosenCheckBoxId = 3;
 
                 saveDoneQuestion();
-                emptyCheckboxes();
+                emptyCheckBoxes(true);
                 break;
             }
 
             case "chbxalternativ4":
             {
                 chbxalternativ4.selectedProperty().set(true);
-
-                chbxalternativ1.selectedProperty().set(false);
-                chbxalternativ2.selectedProperty().set(false);
-                chbxalternativ3.selectedProperty().set(false);
                 chosenCheckBoxId = 4;
 
                 saveDoneQuestion();
-                emptyCheckboxes();
+                emptyCheckBoxes(true);
                 break;
             }
 
         }
     }
 
-    public void saveDoneQuestion()
+    private void saveDoneQuestion()
     {
         System.out.println("CurrentQuestion: " + currentQ.getId());
         StudentAnswer answer = new StudentAnswer(3, currentQ.getId(), chosenCheckBoxId, Date.valueOf(LocalDate.MAX));
@@ -249,32 +235,134 @@ public class StudentTestController implements Initializable {
 //                System.out.println("Answer added");
 //            }
 //        });
-//        if (answers.size() == observableQuestions.size()) {
-//            //byt till startpage för student
-//            try {
-//                // go to create account scene
-//                Stage goToNextStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//                Parent p = FXMLLoader.load(getClass().getResource("createAccount.fxml"));
-//                Scene GoToNextScene = new Scene(p);
-//
-//                goToNextStage.setScene(GoToNextScene);
-//                goToNextStage.setResizable(false);
-//                goToNextStage.setTitle("Create a new account");
-//                goToNextStage.show();
-//
-//            } catch (IOException e) {
-//                System.out.println(e.getCause());
-//            }
-//        }
+        if (answers.size() == observableQuestions.size()) 
+        {
+            btnSaveTestAction();
+        }
     }
 
     @FXML
-    public void lvQuestionsNRSelected()
+    private void lvQuestionsNRSelected()
     {
-        lblcurrentQuestionNR.setText("Question: " + String.valueOf(lvQuestionsNR.getSelectionModel().getSelectedIndex() + 1));
+        emptyCheckBoxes(false);
         currentQuestion();
 
-        System.out.println("Answers: " + answers);
+        fillCheckBox();
+    }
+
+    private void currentQuestion()
+    {
+        // hålla reda på current question
+        currentQ = (Question) lvQuestionsNR.getSelectionModel().getSelectedItem();
+        lblcurrentQuestionNR.setText("Question: " + String.valueOf(lvQuestionsNR.getSelectionModel().getSelectedIndex() + 1));
+        Image image;
+        
+        if (currentQ != null)
+        {
+            lblQuestionText.setText(currentQ.getqText());
+
+            if (currentQ.getImageURL() == null)
+            {
+                currentQ.setImageURL(emptyUrl); //emptyIrl finns definierad längst upp
+            }
+            
+            image = new Image(currentQ.getImageURL());
+            ivImage.setImage(image);
+            
+            for (int i = 0; i < currentQ.getAnswers().size(); i++)
+            {
+                //Dessa rader kommer alltid att få ut värdet på svaret i en sträng
+                String currentQue = currentQ.getAnswers().get(i).toString().substring(20);
+                currentQue = currentQue.substring(0, currentQue.length() - 1);
+
+                switch (i)
+                {
+                    case 0:
+                        quealternativ1.setText(currentQue);
+                        break;
+                    case 1:
+                        quealternativ2.setText(currentQue);
+                        break;
+                    case 2:
+                        quealternativ3.setText(currentQue);
+                        break;
+                    case 3:
+                        quealternativ4.setText(currentQue);
+                        break;
+
+                }
+            }
+            
+            for (int i = 0; i < answers.size(); i++)
+            {
+                System.out.println("For-loop startad");
+                System.out.println("currentQ.getId(): " + currentQ.getId());
+                System.out.println("answers.get(i).getQuestion_Id(): " + answers.get(i).getQuestion_Id());
+                System.out.println("currentQ.getText(): " + currentQ.getqText());
+                if (currentQ.getId() == answers.get(i).getQuestion_Id())
+                {
+                    fillCheckBox();
+                }
+            }
+        }
+    }
+
+    private void startTimer()
+    {
+        //timer for time left
+        lblTimeLeft.textProperty().bind(timeSeconds.asString());
+
+        if (timeline != null)
+        {
+            timeline.stop();
+        }
+        timeSeconds.set(startTime);
+        timeline = new Timeline();
+        timeline.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(startTime + 1),
+                        new KeyValue(timeSeconds, 0)));
+        timeline.playFromStart();
+        
+        timeline.setOnFinished((event) -> 
+        {
+            //Tiden för testet ska gå ut
+            System.out.println("Tiden för testet har gått ut");
+            btnSaveTestAction();
+        });
+    }
+
+    private void emptyCheckBoxes(boolean goToNextQuestion)
+    {
+        if (goToNextQuestion)
+        {
+            lvQuestionsNR.getSelectionModel().select(lvQuestionsNR.getSelectionModel().getSelectedIndex() + 1);
+            currentQuestion();
+        }
+        
+        chbxalternativ1.selectedProperty().set(false);
+        chbxalternativ2.selectedProperty().set(false);
+        chbxalternativ3.selectedProperty().set(false);
+        chbxalternativ4.selectedProperty().set(false);
+    }
+    
+    private List<models.Question> loadQuestions()
+    {
+        List<models.Question> allQuestions = qr.getQuestions();
+        List<models.Question> filteredQuestions = new ArrayList();
+        
+        for (models.Question q : allQuestions)
+        {
+            if (currentTest.getId() == q.getTest_Id())
+            {
+                filteredQuestions.add(q);
+            }
+        }
+        
+        return filteredQuestions;
+    }
+    
+    private void fillCheckBox()
+    {
         for (int i = 0; i < answers.size(); i++)
         {
             if (currentQ.getId() == answers.get(i).getQuestion_Id())
@@ -317,88 +405,17 @@ public class StudentTestController implements Initializable {
         }
     }
 
-    public void currentQuestion()
-    {
-        // hålla reda på current question
-        currentQ = (Question) lvQuestionsNR.getSelectionModel().getSelectedItem();
-        Image image;
-        if (currentQ != null)
-        {
-            lblQuestionText.setText(currentQ.getqText());
-
-            if (currentQ.getImageURL() == null)
-            {
-                currentQ.setImageURL(emptyUrl);
-            }
-            image = new Image(currentQ.getImageURL());
-            ivImage.setImage(image);
-            for (int i = 0; i < currentQ.getAnswers().size(); i++)
-            {
-
-                String currentQue = currentQ.getAnswers().get(i).toString().substring(20);
-                currentQue = currentQue.substring(0, currentQue.length() - 1);
-
-                switch (i)
-                {
-                    case 0:
-                        quealternativ1.setText(currentQue);
-                        break;
-                    case 1:
-                        quealternativ2.setText(currentQue);
-                        break;
-                    case 2:
-                        quealternativ3.setText(currentQue);
-                        break;
-                    case 3:
-                        quealternativ4.setText(currentQue);
-                        break;
-
-                }
-            }
-        }
-    }
-
-    public void startTimer()
-    {
-        //timer for time left
-        lblTimeLeft.textProperty().bind(timeSeconds.asString());
-
-        if (timeline != null)
-        {
-            timeline.stop();
-        }
-        timeSeconds.set(startTime);
-        timeline = new Timeline();
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.seconds(startTime + 1),
-                        new KeyValue(timeSeconds, 0)));
-        timeline.playFromStart();
-        
-        timeline.setOnFinished((event) -> 
-        {
-            //Tiden för testet ska gå ut
-            System.out.println("Tiden för testet har gått ut");
-            btnSaveTestAction();
-        });
-    }
-
-    private void emptyCheckboxes()
-    {
-        lvQuestionsNR.getSelectionModel().select(lvQuestionsNR.getSelectionModel().getSelectedIndex() + 1);
-        currentQuestion();
-
-        chbxalternativ1.selectedProperty().set(false);
-        chbxalternativ2.selectedProperty().set(false);
-        chbxalternativ3.selectedProperty().set(false);
-        chbxalternativ4.selectedProperty().set(false);
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
+        currentTest = StudentStartPageController.currentTest;
+        startTime = currentTest.getTotalTime();
+        timeSeconds = new SimpleIntegerProperty(startTime);
+        chosenCheckBoxId = 0;
+        
         startTimer();
+        questions = loadQuestions();
         populateListView();
-        currentQuestion();
 
         lvQuestionsNR.getSelectionModel().select(0);
         currentQuestion();
