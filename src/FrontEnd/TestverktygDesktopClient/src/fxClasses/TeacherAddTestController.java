b058ad47483216353661dfaabdac1a561fe58dc9
Unknown <ali@faradji.se>
2017-06-07 12:59:57 +0200

    merge fix

--- src/FrontEnd/TestverktygDesktopClient/src/fxClasses/TeacherAddTestController.java
+++ src/FrontEnd/TestverktygDesktopClient/src/fxClasses/TeacherAddTestController.java
@@ -1,7 +1,376 @@
+package fxClasses;
 
-import models.Test;
-import propertymodels.Teacher;
-        t.setSubject(currentTeacher.getSubject());
-        t.setTotalTime(Integer.parseInt(textFieldTimeLeft.getText()));
-        t.setTeacher_id(currentTeacher.getId());
-        System.out.println(t.getTeacher_id() + "---------------------------");
+import static fxClasses.LoginController.currentTeacher;
+
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+
+import javafx.fxml.FXML;
+
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.CheckBox;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.image.ImageView;
+
+
+import repositories.QuestionRepository;
+
+import repositories.TestRepository;
+
+/**
+ *
+ * FXML Controller class
+ *
+ *
+ *
+ * @author Allan
+ *
+ */
+public class TeacherAddTestController implements Initializable {
+
+    TestRepository tr = new TestRepository();
+
+    @FXML
+
+    private Label labelTeacherName;
+
+    @FXML
+
+    private Label labelResponseAlt;
+
+    @FXML
+
+    private Label labelSubject;
+
+    @FXML
+
+    private Label labelSeeResult;
+
+    @FXML
+
+    private Label labelTimeLeft;
+
+    @FXML
+
+    private Label labelQuestionNr;
+
+    @FXML
+
+    private Label labelCheckBoxCorrect;
+
+    @FXML
+
+    private Label labelAddPic;
+
+    @FXML
+
+    private CheckBox chckBxYes;
+
+    @FXML
+
+    private CheckBox chckBxNo;
+
+    @FXML
+
+    private TextField textFieldTimeLeft;
+
+    @FXML
+
+    private TextField textFieldTestName;
+
+    @FXML
+
+    private TextField textFieldQuestionText;
+
+    @FXML
+
+    private TextField textFieldQuestionAlt1;
+
+    @FXML
+
+    private CheckBox checkBxQuestionAlt1;
+
+    @FXML
+
+    private TextField textFieldQuestionAlt2;
+
+    @FXML
+
+    private CheckBox checkBxQuestionAlt2;
+
+    @FXML
+
+    private CheckBox checkBxQuestionAlt3;
+
+    @FXML
+
+    private TextField textFieldQuestionAlt4;
+
+    @FXML
+
+    private CheckBox checkBxQuestionAlt4;
+
+    @FXML
+
+    private TextField textFieldQuestionAlt3;
+
+    @FXML
+
+    private ImageView imageViewImage;
+
+    @FXML
+
+    private TextField textFieldImageUrl;
+
+    @FXML
+
+    private Button btnSave;
+
+    @FXML
+
+    private Button btnDone;
+
+    List<models.Question> newQuestions = new ArrayList();
+
+    ArrayList<String> tempAnswers = new ArrayList<>();
+
+    int idOfLastTestInList;
+
+    int chosenCheckBoxId = 0;
+
+    int questionNumCount = 1;
+
+    int isAutoCorrected;
+
+    @Override
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+        labelQuestionNr.setText("Fråga nummer " + questionNumCount);
+
+        chckBxYes.selectedProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (newValue) {
+
+                chckBxNo.setSelected(false);
+
+                isAutoCorrected = 1;
+
+            }
+
+        });
+
+        chckBxNo.selectedProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (newValue) {
+
+                chckBxYes.setSelected(false);
+
+                isAutoCorrected = 0;
+
+            }
+
+        });
+
+        //setLabelsOfTeacher();
+    }
+
+    @FXML
+
+    private void checkBoxHandler(MouseEvent event) {
+
+        String source1 = event.getSource().toString().substring(12, 31);
+
+        switch (source1) {
+
+            case "checkBxQuestionAlt1": {
+
+                checkBxQuestionAlt1.selectedProperty().set(true);
+
+                checkBxQuestionAlt2.selectedProperty().set(false);
+
+                checkBxQuestionAlt3.selectedProperty().set(false);
+
+                checkBxQuestionAlt4.selectedProperty().set(false);
+
+                chosenCheckBoxId = 1;
+
+                break;
+
+            }
+
+            case "checkBxQuestionAlt2": {
+
+                checkBxQuestionAlt2.selectedProperty().set(true);
+
+                checkBxQuestionAlt1.selectedProperty().set(false);
+
+                checkBxQuestionAlt3.selectedProperty().set(false);
+
+                checkBxQuestionAlt4.selectedProperty().set(false);
+
+                chosenCheckBoxId = 2;
+
+                break;
+
+            }
+
+            case "checkBxQuestionAlt3": {
+
+                checkBxQuestionAlt3.selectedProperty().set(true);
+
+                checkBxQuestionAlt1.selectedProperty().set(false);
+
+                checkBxQuestionAlt2.selectedProperty().set(false);
+
+                checkBxQuestionAlt4.selectedProperty().set(false);
+
+                chosenCheckBoxId = 3;
+
+                break;
+
+            }
+
+            case "checkBxQuestionAlt4": {
+
+                checkBxQuestionAlt4.selectedProperty().set(true);
+
+                checkBxQuestionAlt1.selectedProperty().set(false);
+
+                checkBxQuestionAlt2.selectedProperty().set(false);
+
+                checkBxQuestionAlt3.selectedProperty().set(false);
+
+                chosenCheckBoxId = 4;
+
+                break;
+
+            }
+
+        }
+
+        labelTeacherName.setText(source1);
+
+    }
+
+    public void setLabelsOfTeacher() {
+
+        labelTeacherName.setText(currentTeacher.getName());
+
+        labelSubject.setText(currentTeacher.getSubject());
+
+    }
+
+    @FXML
+
+    private void btnSaveAction(ActionEvent event) {
+
+        models.Question q = new models.Question();
+
+        q.setqText(textFieldQuestionText.getText());
+
+        tempAnswers.add(textFieldQuestionAlt1.getText());
+
+        tempAnswers.add(textFieldQuestionAlt2.getText());
+
+        tempAnswers.add(textFieldQuestionAlt3.getText());
+
+        tempAnswers.add(textFieldQuestionAlt4.getText());
+
+        q.setImageURL(textFieldImageUrl.getText());
+
+        q.setAnswers(tempAnswers);
+
+        if (chosenCheckBoxId > 0) {
+
+            q.setCorrectAnswer(chosenCheckBoxId - 0);//om denna variabel ska visa vart i arrayen som svaret finns så måste det bli ett minus 1?
+
+            newQuestions.add(q);
+
+            textFieldQuestionText.clear();
+
+            textFieldQuestionAlt1.clear();
+
+            textFieldQuestionAlt2.clear();
+
+            textFieldQuestionAlt3.clear();
+
+            textFieldQuestionAlt4.clear();
+
+            checkBxQuestionAlt1.selectedProperty().set(false);
+
+            checkBxQuestionAlt2.selectedProperty().set(false);
+
+            checkBxQuestionAlt3.selectedProperty().set(false);
+
+            checkBxQuestionAlt4.selectedProperty().set(false);
+
+        } else {
+
+            System.out.println("Felmeddelande i GUI");
+
+        }
+
+        questionNumCount++;
+
+    }
+
+    @FXML
+
+    private void btnDoneAction(ActionEvent event) {
+
+        QuestionRepository qr = new QuestionRepository();
+
+        models.Test t = new models.Test();
+
+        t.setName(textFieldTestName.getText());
+
+        t.setAutoCorrectedTest(isAutoCorrected);
+
+        t.setSubject(LoginController.currentTeacher.getSubject());
+
+        int totalTime = Integer.parseInt(textFieldTimeLeft.getText());
+
+        totalTime = totalTime * 60;
+
+        t.setTotalTime(totalTime);
+
+        models.Test newTest = tr.addTest(currentTeacher.getId(),t);
+
+        for (int i = 0; i < newQuestions.size(); i++) {
+
+            models.Question q = new models.Question();
+
+            q.setqText(newQuestions.get(i).getqText());
+
+            q.setAnswers(newQuestions.get(i).getAnswers());
+
+            q.setImageURL(newQuestions.get(i).getImageURL());
+
+            q.setCorrectAnswer(newQuestions.get(i).getCorrectAnswer());
+
+            q.setTest_Id(newTest.getId());
+
+            qr.addQuestion(newTest.getId(), q);
+
+        }
+
+    }
+
+}
--- src/FrontEnd/TestverktygDesktopClient/src/propertymodels/Participant.java
+++ src/FrontEnd/TestverktygDesktopClient/src/propertymodels/Participant.java
@@ -12,64 +12,6 @@ public class Participant{
     StringProperty password;
     StringProperty DTYPE;
 
-
-    public Participant() {
-    }
-
-    public Participant(int id, String name,String DTYPE, String password) {
-        this.id = new SimpleIntegerProperty(id);
-        this.name = new SimpleStringProperty(name);
-        this.password = new SimpleStringProperty(password);
-        this.DTYPE = new SimpleStringProperty(DTYPE);
-    }
-
-    public int getId() {
-        return id.get();
-    }
-
-    public void setId(int id) {
-        this.id.set(id);
-    }
-
-    public String getName() {
-        return name.get();
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
-    public String getPassword() {
-        return password.get();
-    }
-
-    public void setPassword(String password) {
-        this.password.set(password);
-    }
-
-    public String getDTYPE() {
-        return DTYPE.get();
-    }
-
-    public void setDTYPE(String DTYPE) {
-        this.DTYPE.set(DTYPE);
-    }
-
-}
-
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import org.eclipse.persistence.jpa.jpql.parser.StringPrimaryBNF;
-
-public class Participant{
-
-    IntegerProperty id;
-    StringProperty name;
-    StringProperty password;
-    StringProperty DTYPE;
-
     public Participant() {
     }
 
